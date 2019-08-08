2c34140a4fac359296c5ab44b6913ac6acb29ed5
Tommy Feldt <tommyfeldt@gmail.com>
2019-08-08 16:47:09 +0200

    Minor css class changes and styling modifications

--- src/js/fymmot-datepicker.js
+++ src/js/fymmot-datepicker.js
@@ -192,7 +192,7 @@
 
 	A11ydate.prototype.generateCalendarHeader = function(){
 		//Generate Month buttons and heading
-		var calendarHeaderNav = $('<div class="month-nav__wrapper"><button class="btn month-nav__sides left">Previous<span class="visually-hidden"> month</span></button><h2 class="h3" id="month-label" aria-live="assertive" class="">' + this.months[+this.calendarDates[0].month-1] + ' ' + this.calendarDates[0].year + '</h3> <button class="btn month-nav__sides right">Next<span class="visually-hidden"> month</span></i></button> </div>');
+		var calendarHeaderNav = $('<div class="month-nav__wrapper"><button class="btn month-nav__sides__btn left">Previous<span class="visually-hidden"> month</span></button><h2 class="h3" id="month-label" aria-live="assertive" class="">' + this.months[+this.calendarDates[0].month-1] + ' ' + this.calendarDates[0].year + '</h3> <button class="btn month-nav__sides__btn right">Next<span class="visually-hidden"> month</span></i></button> </div>');
 		$('#datepicker_wrapper').prepend(calendarHeaderNav);
 
 		//Generate table headings with weekdays
@@ -213,10 +213,10 @@
 			closeID: 'closeKbdModal'
 		}
 
-		var $keyboard_shortcuts = $('<div class="keyboard-shortcuts-link"><button id="open-keyboard-shortcuts">Show keyboard shortcuts</button></div>');
+		var $keyboard_shortcuts = $('<div class="keyboard-shortcuts-link"><button id="open-keyboard-shortcuts">Keyboard and screen reader instructions</button></div>');
 		$("#datepicker_wrapper").find(".month-nav__wrapper").after($keyboard_shortcuts);
 
-		var modalContent = '<h3 class="kbd_modal__heading" id="kbd_modal_heading" tabindex="0">Keyboard shortcuts</h3><ul><li><p><kbd>TAB</kbd> into the calendar.</p></li><li><p><kbd>LEFT</kbd>/ <kbd>RIGHT</kbd> to change day.</p></li><li><p><kbd>UP</kbd>/ <kbd>DOWN</kbd> to change week.</p></li><li><p><kbd>HOME</kbd>/ <kbd>END</kbd> for first or last day of the month.</p></li><li><p><kbd>PAGE UP</kbd>/ <kbd>PAGE DOWN</kbd> for the same day in the previous or next month.</p></li><li><p><kbd>SPACE</kbd> to select a date.</p></li></ul><h3 class="kbd_modal__heading">With screen readers</h3><ul><li><p>By default, you can browse between days in the calendar using the down arrow key, by button list, or by searching for a specific button.</p></li><li><p>You can activate forms mode manually in NVDA and JAWS to make use of the keyboard shortcuts</p></li></ul>'
+		var modalContent = '<h3 class="kbd_modal__heading" id="kbd_modal_heading" tabindex="0">Keyboard shortcuts</h3><ul><li><p><kbd>TAB</kbd> into the calendar.</p></li><li><p><kbd>LEFT</kbd>/ <kbd>RIGHT</kbd> to change day.</p></li><li><p><kbd>UP</kbd>/ <kbd>DOWN</kbd> to change week.</p></li><li><p><kbd>HOME</kbd>/ <kbd>END</kbd> for first or last day of the month.</p></li><li><p><kbd>PAGE UP</kbd>/ <kbd>PAGE DOWN</kbd> for the same day in the previous or next month.</p></li><li><p><kbd>SPACE</kbd> to select a date.</p></li></ul><h3 class="kbd_modal__heading">With screen readers</h3><ul><li><p>By default, you can browse between days in the calendar using the down arrow key. You can also use a button list, or free text search for a specific button.</p></li><li><p>If you want to use keyboard shortcuts with JAWS or NVDA, you need to activate application mode manually</p></li></ul>'
 
 		var $kbd_modal = $(
 			'<div id="keyboard_shortcuts_modal" hidden role="dialog" aria-modal="false" aria-labelledby="kbd_modal_heading"><div class="keyboard_modal__inner"> '+ modalContent + '</div><div class="keyboard_modal__bottom"><button id="closeKbdModal" class="button large">Got it!</button></div></div>');
@@ -224,6 +224,7 @@
 		//Add keyboard modal to dom
 		$("#datepicker_wrapper").prepend($kbd_modal);
 
+		//Initialize keyboard modal using 
 		initializeKeyboardModal(idvalues.openID, idvalues.dialogID, idvalues.closeID);
 	}
 
@@ -386,14 +387,14 @@
 	 */
 	A11ydate.prototype.registerClickListeners = function(){
 
-		$(".btn.month-nav__sides.right").click(function(){
+		$(".btn.month-nav__sides__btn.right").click(function(){
 			that.incrementMonth(undefined, 1);
-			$(".btn.month-nav__sides.right").focus();
+			$(".btn.month-nav__sides__btn.right").focus();
 		})
 
-		$(".btn.month-nav__sides.left").click(function(){
+		$(".btn.month-nav__sides__btn.left").click(function(){
 			that.incrementMonth(undefined, -1);
-			$(".btn.month-nav__sides.left").focus();
+			$(".btn.month-nav__sides__btn.left").focus();
 		})
 
 		$('td.disabled').mousedown(function(){
@@ -425,7 +426,7 @@
 
 		function addChips(){
 			//Add the chips
-			var chips = '<ul class="chip-set"><li><button class="chip">Yesterday</button></li><li><button class="chip">Tomorrow</button></li><li><button class="chip">Next week</button></li><li><button class="chip">In 30 days</button></li></ul>'
+			var chips = '<ul aria-label="Quick links" class="chip-set"><li><button class="chip">Yesterday</button></li><li><button class="chip">Tomorrow</button></li><li><button class="chip">Next week</button></li><li><button class="chip">In 30 days</button></li></ul>'
 			$("#date-error").attr("aria-live", "assertive").before(chips);
 
 			$("#input_wrapper .chip").click(function(){
@@ -911,7 +912,7 @@
 
 
 		var label = cell.attr("aria-label");
-		var role = 'button';
+		var role = 'toggle button';
 		if (cell.attr('aria-pressed') == "true"){
 				var pressed = ", pressed" 
 			} else {
